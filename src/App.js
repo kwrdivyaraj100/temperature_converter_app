f1303a3d447af63b12ff86cb4d3edf108bb90872
Divya R <kwrdivyaraj100@gmail.com>
2023-11-02 12:37:44 +0530

    Have return skeleton code for parent component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
 
 function App() {
+
+  const celsius = 0;
+  const fahrenheit = 0;
+  const kelvin = 0;
+
+  function onCelsiusChange(){
+
+  }
+
+  function onFahrenheitChange(){
+    
+  }
+  function onKelvinChange(){
+    
+  }
+
   return (
-    <Demo/>
+    <div style={{"textAlign": "center"}}>
+      <h2>Celsius: <Child temperature = {celsius} onTemperatureChange = {onCelsiusChange}/></h2>
+      <h2>Fahrenheit: <Child temperature = {fahrenheit} onTemperatureChange = {onFahrenheitChange}/></h2>
+      <h2>kelvin: <Child temperature = {kelvin} onTemperatureChange = {onKelvinChange}/></h2>
+    </div>
   );
 }
 
